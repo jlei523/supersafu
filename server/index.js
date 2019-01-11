bdb1a5698fa1904390431114b52ad29f4ccb41a7
jlei523 <jlei523@gmail.com>
2019-01-11 19:10:02 +0800

    first commit

---
+++ server/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const next = require("next");
+const bodyParser = require("body-parser");
+const compression = require("compression");
+
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+  server.use(bodyParser.json()); // bodyParser to handle POST requests
+  server.use(bodyParser.urlencoded({ extended: true }));
+
+  server.get("/a", (req, res) => {
+    return app.render(req, res, "/b", req.query);
+  });
+
+  server.get("/b", (req, res) => {
+    return app.render(req, res, "/a", req.query);
+  });
+
+  server.get("/posts/:id", (req, res) => {
+    return app.render(req, res, "/posts", { id: req.params.id });
+  });
+
+  server.get("*", (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(port, err => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
